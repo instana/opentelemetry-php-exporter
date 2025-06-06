34ca98491ded3c617d07c5ab504c7e205879a498
Heena Bansal <139580694+HeenaBansal20@users.noreply.github.com>
2025-06-06 00:36:39 -0400

    Update sdk.php

--- examples/sdk.php
+++ examples/sdk.php
@@ -27,7 +27,7 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, true); 
     
-$response=    curl_exec($ch);
+$response = curl_exec($ch);
 curl_close($ch);
 echo $response;
 
